6870df4fc1a4802dc247e3c21e1300ab44456fb6
Gak <=>
2025-06-22 20:06:36 -0400

    True end Day 4, remove mailscp 3d2d drawing check since removed 3d2d drawing.

--- lua/entities/mailscp/cl_init.lua
+++ lua/entities/mailscp/cl_init.lua
@@ -2,16 +2,4 @@ include("shared.lua")
 
 function ENT:Draw()
     self:DrawModel()
-
-    local ply = LocalPlayer()
-    if not IsValid(ply) then
-        return
-    end
-
-    local dist = self:GetPos():Distance2DSqr(ply:GetPos())
-    local max = 100000
-
-    if dist > max then 
-        return 
-    end
 end
